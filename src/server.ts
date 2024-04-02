426dea1ce8cd288ff94e2d50472a740503f8a1ad
Natan <natan.devback@gmail.com>
2024-04-02 19:34:08 -0300

    subindo o código da api com prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
-
-app.get("/", (Request, Reply) => {
-	return "hello world";
+const prisma = new PrismaClient({
+	log: ["query"],
 });
-app.listen({ port: 3000 | 8080 }).then(() => {
+
+app.post("/events", async (request, reply) => {
+	const createEventsSchema = z.object({
+		title: z.string().min(4),
+		details: z.string().nullable(),
+		maximunAttendees: z.number().int().positive().nullable(),
+	});
+
+	const data = createEventsSchema.parse(request.body);
+
+	const  event = await prisma.event.create({
+		data: {
+			title: data.title,
+			details: data.details,
+			maximunAttendees: data.maximunAttendees,
+			slug: new Date().toISOString(),
+		},
+	});
+	return reply.status(201).send({eventId:event.id})
+});
+app.listen({ port: 3000 }).then(() => {
 	console.log("app running");
 });
